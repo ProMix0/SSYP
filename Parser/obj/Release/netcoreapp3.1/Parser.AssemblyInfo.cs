36aa70d51a169844288d2615733fe7f011fdd738
Михаил Завёрткин <misha.zavertkin@mail.ru>
2020-07-16 16:05:05 +0700

    Sln added

--- Parser/obj/Release/netcoreapp3.1/Parser.AssemblyInfo.cs
+++ Parser/obj/Release/netcoreapp3.1/Parser.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Programm")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Parser")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Programm")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Programm")]
+[assembly: System.Reflection.AssemblyProductAttribute("Parser")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Parser")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
